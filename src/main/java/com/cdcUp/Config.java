3f2d07a6017598704ebeb317ffbe8a962606a482
于佳骥 <yjj@datacyber.com>
2025-04-22 10:12:17 +0800

    1.1添加日志打印储存工具和配置文件选项

---
+++ src/main/java/com/cdcUp/Config.java
@@ -0,0 +1,42 @@
+package com.cdcUp;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Config {
+    public static String MYSQL_HOST;
+    public static int MYSQL_PORT;
+    public static String MYSQL_USERNAME;
+    public static String MYSQL_PASSWORD;
+    public static String HIVE_METASTORE_THRIFT_URL;
+    public static String HDFS_URL;
+
+    // Method to load configuration from JSON file
+    static {
+        loadFromJson();
+    }
+
+    private static void loadFromJson() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\eclipse\\workspace\\CDCSchemaUpdater\\src\\main\\java\\com\\cdcUp\\config.json"))) {
+            StringBuilder contentBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line);
+            }
+            String jsonContent = contentBuilder.toString();
+            JSONObject jsonObject = JSON.parseObject(jsonContent);
+            Config.MYSQL_HOST = jsonObject.getString("MYSQL_HOST");
+            Config.MYSQL_PORT = jsonObject.getIntValue("MYSQL_PORT");
+            Config.MYSQL_USERNAME = jsonObject.getString("MYSQL_USERNAME");
+            Config.MYSQL_PASSWORD = jsonObject.getString("MYSQL_PASSWORD");
+            Config.HIVE_METASTORE_THRIFT_URL = jsonObject.getString("HIVE_METASTORE_THRIFT_URL");
+            Config.HDFS_URL = jsonObject.getString("HDFS_URL");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load configuration from JSON file", e);
+        }
+    }
+}
